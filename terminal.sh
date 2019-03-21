7f7f9fbb9517c897b8a9720c80e1811fb2a00393
Martin Juhasz <info@martinjuhasz.de>
2019-03-21 16:33:20 +0100

    fix(folders): correct folder creation

--- terminal.sh
+++ terminal.sh
@@ -2,10 +2,10 @@
 brew cask install iterm2
 mkdir ~/.terminal
 curl -L https://iterm2.com/shell_integration/bash -o ~/.terminal/iterm2_shell_integration.bash
-(cd .terminal && git clone https://github.com/rupa/z.git)
+(cd ~/.terminal && git clone https://github.com/rupa/z.git)
 brew cask install font-fira-mono-for-powerline
 pip install powerline-shell
-mkdir ~/Library/Application\ Support/iTerm2/DynamicProfiles/
+mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 cp iterm_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 mkdir -p ~/.config/powerline-shell
 cp powerline_config.json ~/.config/powerline-shell/config.json
